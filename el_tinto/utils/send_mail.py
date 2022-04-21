ab814ba181b55ec6cacc8e2bb3a34f192c7a2123
CharlieBGood <carlosbueno1196@gmail.com>
2022-04-21 11:39:07 -0500

    fix: send emails addresses as lists

--- el_tinto/utils/send_mail.py
+++ el_tinto/utils/send_mail.py
@@ -2,7 +2,7 @@ from django.core.mail import EmailMessage
 from django.template import loader
 
 
-def send_email(subject, html_file, mail_data, email, reply_to=None):
+def send_email(subject, html_file, mail_data, emails, reply_to=None):
     """Send email from template."""
     template = loader.get_template(
         f'../templates/mailings/{html_file}'
@@ -10,11 +10,11 @@ def send_email(subject, html_file, mail_data, email, reply_to=None):
     html = template.render(mail_data)
     if reply_to:
         message_user = EmailMessage(
-            subject, html, 'El Tinto <info@eltinto.xyz>', [email, ], reply_to=[reply_to, ]
+            subject, html, 'El Tinto <info@eltinto.xyz>', emails, reply_to=[reply_to, ]
         )
     else:
         message_user = EmailMessage(
-            subject, html, 'El Tinto <info@eltinto.xyz>', [email, ]
+            subject, html, 'El Tinto <info@eltinto.xyz>', emails
         )
     message_user.content_subtype = 'html'
     message_user.send(fail_silently=False)

4e73a1789a5e74054f82b5c9513c81cc764cf0ef
CharlieBGood <carlosbueno1196@gmail.com>
2022-05-04 15:45:09 -0500

    fix: configuration set header

--- el_tinto/utils/send_mail.py
+++ el_tinto/utils/send_mail.py
@@ -33,12 +33,12 @@ def send_email(subject, html_file, mail_data, emails, reply_to=None):
     if reply_to:
         message_user = EmailMessage(
             subject, html, 'El Tinto <info@eltinto.xyz>', emails, reply_to=[reply_to, ],
-            headers={'X-SES-CONFIGURATION-SET': 'test', 'X-SES-MESSAGE-TAGS': 'Email=Null'}
+            headers={'X-SES-CONFIGURATION-SET': 'el_tinto_daily_email', 'X-SES-MESSAGE-TAGS': 'Email=Null'}
         )
     else:
         message_user = EmailMessage(
             subject, html, 'El Tinto <info@eltinto.xyz>', emails,
-            headers={'X-SES-CONFIGURATION-SET': 'test', 'X-SES-MESSAGE-TAGS': 'Email=Null'}
+            headers={'X-SES-CONFIGURATION-SET': 'el_tinto_daily_email', 'X-SES-MESSAGE-TAGS': 'Email=Null'}
         )
     message_user.content_subtype = 'html'
     message_user.send(fail_silently=False)

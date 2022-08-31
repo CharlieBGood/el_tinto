85a5a5794d2f51da4433d0d7b0d6a09ad4bcc84d
CharlieBGood <carlosbueno1196@gmail.com>
2022-08-31 18:17:10 -0500

    test sending email address.

--- el_tinto/config/development.py
+++ el_tinto/config/development.py
@@ -4,7 +4,7 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 
-class Production(Common):
+class Development(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
     SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
     # Site

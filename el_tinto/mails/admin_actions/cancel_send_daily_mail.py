b1a1cd00e5c0cf2cf6dd7a4e2a5b4df14707b1aa
CharlieBGood <carlosbueno1196@gmail.com>
2022-09-20 11:56:17 -0500

    fix cancel email sending

--- el_tinto/mails/admin_actions/cancel_send_daily_mail.py
+++ el_tinto/mails/admin_actions/cancel_send_daily_mail.py
@@ -28,6 +28,8 @@ def cancel_send_daily_email(_, request, queryset):
     if not mail.sent_datetime:
         scheduler = get_scheduler()
         scheduler.remove_job(str(mail.id))
+        scheduler.remove_job(f'{str(mail.id)}_check')
+        scheduler.start()
         mail.programmed = False
         mail.save()
 

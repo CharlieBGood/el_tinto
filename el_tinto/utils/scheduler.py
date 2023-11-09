e3c606316a8dec0bdd51ccf59f1912a48e42ec74
CharlieBGood <carlosbueno1196@gmail.com>
2023-11-09 08:15:25 -0500

    fix misfire_grace_time

--- el_tinto/utils/scheduler.py
+++ el_tinto/utils/scheduler.py
@@ -5,6 +5,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
+MISFIRE_GRACE_TIME = 60*60*2,  # 60 s * 60 m * 2h
+
 JOBSTORES = {
     'default': SQLAlchemyJobStore(url=settings.DATABASE_FULL_URL)
 }
@@ -14,19 +16,17 @@ EXECUTORS = {
 }
 JOB_DEFAULTS = {
     'coalesce': False,
-    'max_instances': 3
+    'max_instances': 3,
+    'misfire_grace_time': MISFIRE_GRACE_TIME
 }
 
 TIMEZONE = timezone('America/Bogota')
-
-MISFIRE_GRACE_TIME = 60*60*2,  # 60 s * 60 m * 2h
 
 scheduler = BackgroundScheduler(
     jobstores=JOBSTORES,
     executors=EXECUTORS,
     job_defaults=JOB_DEFAULTS,
     timezone=TIMEZONE,
-    misfire_grace_time=MISFIRE_GRACE_TIME
 )
 
 scheduler.start()
